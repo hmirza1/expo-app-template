da7a5e385acd46cedc1bc7bfdcda394c8d472675
hmirza1 <mirzaaa12@gmail.com>
2025-06-08 05:55:20 +0100

    re-animated test

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,12 +1,52 @@
 import { Link } from 'expo-router';
+import React from 'react';
 import { Text, View } from 'react-native';
+import Animated, {
+  useAnimatedStyle,
+  useSharedValue,
+  withRepeat,
+  withSpring,
+} from 'react-native-reanimated';
 
 export default function ChatsScreen() {
+  const scale = useSharedValue(1);
+  const rotation = useSharedValue(0);
+
+  React.useEffect(() => {
+    scale.value = withRepeat(
+      withSpring(1.2, {
+        damping: 2,
+        mass: 20,
+        overshootClamping: false,
+        restDisplacementThreshold: 0.01,
+        restSpeedThreshold: 2,
+        stiffness: 80,
+      }),
+      -1,
+      true,
+    );
+    rotation.value = withRepeat(
+      withSpring(5, {
+        damping: 3,
+        stiffness: 100,
+      }),
+      -1,
+      true,
+    );
+  }, [scale, rotation]);
+
+  const animatedStyle = useAnimatedStyle(() => {
+    return {
+      transform: [{ scale: scale.value }, { rotate: `${rotation.value}deg` }],
+    } as any;
+  });
   return (
     <View className="flex-1 items-center justify-center bg-claude">
-      <Text className="mb-4 text-3xl font-bold text-slate-900">
-        NativeWind Test
-      </Text>
+      <Animated.View style={animatedStyle}>
+        <Text className="mb-4 text-3xl font-bold text-slate-900">
+          NativeWind Test
+        </Text>
+      </Animated.View>
 
       <Link href="/chat/drsyed" push>
         <Text className="text-destructive text-2xl font-bold underline">
